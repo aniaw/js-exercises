41f6c7ffbc0c33b107071b89ff4e84a3b530c008
Michal Niedziela <sunday1601@gmail.com>
2016-11-29 10:29:59 +0100

    Initial project with gulp, less and browser sync

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+/**
+ * Created by sunday on 11/29/16.
+ */
+var gulp = require('gulp');
+var less = require('gulp-less');
+var browserSync = require('browser-sync').create();
+
+
+
+gulp.task('less', function(){
+    return gulp.src('app/less/**/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+        stream: true
+    }))
+});
+
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        }
+    })
+})
+
+gulp.task('watch', ['browserSync','less'], function () {
+    gulp.watch('app/less/**/*.less', ['less']);
+    gulp.watch('app/**/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
+})
