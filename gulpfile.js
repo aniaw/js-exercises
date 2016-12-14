adaf1208949c79b09e2cc4b6278d615e2253a398
student <michal.niedziela@labnoratory.com>
2016-12-14 14:44:48 +0100

    Fix modal to gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -4,28 +4,37 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var browserSync = require('browser-sync').create();
+var ghPages = require('gulp-gh-pages');
 
-
-
-gulp.task('less', function(){
-    return gulp.src('app/less/**/*.less')
-        .pipe(less())
-        .pipe(gulp.dest('app/css'))
-        .pipe(browserSync.reload({
-        stream: true
-    }))
+gulp.task('deploy', function ()
+{
+    return gulp.src('app/**/*')
+            .pipe(ghPages());
 });
 
-gulp.task('browserSync', function () {
+
+gulp.task('less', function ()
+{
+    return gulp.src('app/less/**/*.less')
+            .pipe(less())
+            .pipe(gulp.dest('app/css'))
+            .pipe(browserSync.reload({
+                stream: true
+            }));
+});
+
+gulp.task('browserSync', function ()
+{
     browserSync.init({
         server: {
             baseDir: 'app'
         }
-    })
-})
+    });
+});
 
-gulp.task('watch', ['browserSync','less'], function () {
+gulp.task('watch', ['browserSync', 'less'], function ()
+{
     gulp.watch('app/less/**/*.less', ['less']);
     gulp.watch('app/**/*.html', browserSync.reload);
     gulp.watch('app/js/**/*.js', browserSync.reload);
-})
+});
